759b8ec1850357d9dd32f4deb1002b2bdd38a035
Pratik Deolasi <pratikpd00@gmail.com>
2019-09-17 13:11:36 -0400

    fixed wrong link being created

--- quick_toon.py
+++ quick_toon.py
@@ -16,8 +16,6 @@ def add_shader(material: Material):
     """
     #obtains and creates the nodes necessary
     tree = material.node_tree
-    output_node = tree.nodes[tree.active_output]
-    material_node = tree.nodes[tree.active_input]
     to_RGB_node = tree.nodes.new("ShaderNodeShaderToRGB")
     color_ramp_node = tree.nodes.new("ShaderNodeValToRGB")
     outline_node = tree.nodes.new("ShaderNodeLayerWeight")
@@ -29,11 +27,12 @@ def add_shader(material: Material):
 
     #removes link from input to output
     material_link = links[0]
+    output_socket = material_link.to_socket
     material_socket = material_link.from_socket
     links.remove(material_link)
 
     #creates new links
-    links.new(output_node.inputs[0], mix_node.outputs[0])
+    links.new(output_socket, mix_node.outputs[0])
     #layer weight
     links.new(mix_node.inputs[1], outline_ramp_node.outputs[0])
     links.new(outline_ramp_node.inputs[0], outline_node.outputs[1])
