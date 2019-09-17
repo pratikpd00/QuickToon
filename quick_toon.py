078109ec06dcf9a32ce587d9f996c8db27dd838b
Pratik Deolasi <pratikpd00@gmail.com>
2019-09-17 12:33:36 -0400

    add_shader now generaates links

--- quick_toon.py
+++ quick_toon.py
@@ -23,8 +23,24 @@ def add_shader(material: Material):
     outline_node = tree.nodes.new("ShaderNodeLayerWeight")
     outline_ramp_node = tree.nodes.new("ShaderNodeValToRGB")
     mix_node = tree.nodes.new("ShaderNodeMixRGB")
+
+    #the links in the node tree
+    links = tree.links
+
     #removes link from input to output
-    tree.links.remove(tree.links[0])
+    material_link = links[0]
+    material_socket = material_link.from_socket
+    links.remove(material_link)
+
+    #creates new links
+    links.new(output_node.inputs[0], mix_node.outputs[0])
+    #layer weight
+    links.new(mix_node.inputs[1], outline_ramp_node.outputs[0])
+    links.new(outline_ramp_node.inputs[0], outline_node.outputs[1])
+    #shader to rgb
+    links.new(mix_node.inputs[2], color_ramp_node.outputs[0])
+    links.new(color_ramp_node.inputs[0], to_RGB_node.outputs[0])
+    links.new(to_RGB_node.inputs[0], material_socket)
 
 
 
