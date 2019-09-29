5a7d07e954ed1ea7c02e5e4dd3493d0578e6c1f9
Pratik Deolasi <pratikpd00@gmail.com>
2019-09-28 23:40:30 -0400

    "basic gui"

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 import bpy
-from quick_toon import add_shader
+from . import quick_toon
 from bpy.props import FloatVectorProperty, IntProperty
 
 bl_info = {
@@ -8,7 +8,7 @@ bl_info = {
     "description": "An addon that quickly generates toon shaders",
     "blender": (2, 80, 0),
     "version": (0, 0, 1),
-    "category": "Generic"
+    "category": "Material"
 }
 
 class GenerateShaderOperator(bpy.types.Operator):
@@ -32,17 +32,39 @@ class GenerateShaderOperator(bpy.types.Operator):
     shades: IntProperty(
         name="shades",
         description="the number of shades in the shader",
+        default=2,
         soft_min=2,
         soft_max=10
     )
 
     def execute(self, context):
-        add_shader(material=context.material, start_color=self.start_shade, end_color=self.end_shade, shades=self.shades)
+        quick_toon.add_shader(material=context.material, start_color=self.start_shade, end_color=self.end_shade, shades=self.shades)
         return {'FINISHED'}
 
+
+class ToonMenu(bpy.types.Panel):
+    bl_idname = "TOON_SHADER_generate"
+    bl_label = "Generate toon shader"
+    bl_space_type = 'PROPERTIES'
+    bl_region_type = 'WINDOW'
+    bl_context = "material"
+
+    def draw(self, context):
+        layout = self.layout
+        layout.operator("object.generateshader", text="generate shader")
+
+
+classes = (GenerateShaderOperator, ToonMenu)
+
 def register():
-    ...
+    for c in classes:
+        bpy.utils.register_class(c)
 
 
 def unregister():
-    ...
+    for c in classes:
+        bpy.utils.unregister_class(c)
+
+
+if __name__ is "__main__":
+    register()
--- quick_toon.py
+++ quick_toon.py
@@ -1,3 +1,4 @@
+
 import bpy
 from mathutils import *
 D = bpy.data
